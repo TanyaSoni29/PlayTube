c4ed63eb2c5dc1233cfc90486622a82d1d3c167e
unknown <tanu139sona@gmail.com>
2024-06-04 17:04:11 +0530

    index file is change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
-import { app } from "./app";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB().then(() => {
+    app.on("error", (err) => {
+        console.log("Connection error", err)
+        throw new err;
+    })
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
     })
