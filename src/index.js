40e9514af1c7652dcbfaab7a272bf4e6cdd9c241
unknown <tanu139sona@gmail.com>
2024-06-04 15:13:06 +0530

    userController completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import mongoose from "mongoose";
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+import { app } from "./app";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
+    })
+}).catch((error) => {
+    console.log("DB connection Faces some issuse", error)
+})
+
+
+
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 
 const userSchema = new Schema({
   username: {
@@ -49,6 +52,44 @@ const userSchema = new Schema({
 {
     timestamps: true
 })
+
+userSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next();
+})
+
+userSchema.method.isPasswordCorrect = async function(password) {
+return await bcrypt.compare(password, this.password);
+}
+
+userSchema.method.generateAccessToken = function() {
+  return jwt.sign(
+    {
+      _id : this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName
+    },
+      process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIREIN
+    }
+  )
+}
+userSchema.method.generateRefreshToken = function() {
+  return jwt.sign(
+    {
+      _id : this._id,
+    },
+      process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIREIN
+    }
+  )
+}
+
 
 
 export const User = mongoose.model("User", userSchema);
