0481c941b28a04795b2c6ddae1f2284f7dc52c11
TanyaSoni2912 <tanu139sona@gmail.com>
2025-05-26 12:01:30 +0530

    health Check and tweet route added

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,12 +1,15 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+const healthCheck = asyncHandler(async (req, res) => {
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Service is healthy"))
+    .catch((err) => {
+      throw new ApiError(500, "Internal Server Error", err.message);
+    });
+});
 
-const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
-
-export {
-    healthcheck
-    }
+export { healthcheck };
